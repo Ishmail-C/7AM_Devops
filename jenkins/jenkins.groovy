fc555ba7a93f3120321c6df7b0ebda4e4d5b50ae
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2022-06-22 10:44:28 +0000

    new function is added

--- jenkins/jenkins.groovy
+++ jenkins/jenkins.groovy
@@ -6,13 +6,18 @@ pipeline {
 								      sh '''
 								      myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
  								      echo "My WAN/Public IP address: ${myip}"
-								      echo '${df}'
-								      echo "${hostname}"
 								      '''
 								     }
               							}                      
                      }
+			stage('first stage is printing the ip'){
+								steps{
+								      sh '''
+                                                                      myip="$(ps)"
+                                                                      echo "process: ${myip}"
+                                                                      '''
+							         	}
+								}		 
 
-
-	}
+	 	}
 
