64d00443a91da0c2735168ba1f245dc27a8ccb24
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2022-06-22 09:57:26 +0000

    Pipe line job is created in jenkins

---
+++ jenkins/jenkins.groovy
@@ -0,0 +1,16 @@
+pipeline {
+	agent any
+		stages{
+			stage(first stage is printing the ip){
+								steps{
+								      sh '''
+								      myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+ 								      echo "My WAN/Public IP address: ${myip}"
+								      '''
+								     }
+              							}                      
+                     }
+
+
+	}
+
